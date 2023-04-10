f0fce731bfbef4a8ff21fb7f59b0dcb712108fec
ArtemMartynow <artem1435@i.ua>
2023-04-11 02:48:47 +0300

    added script for slider

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function removeClass() {
 }
 
 
+let leftBtnSlider = document.getElementById('tasty-pancakes-content-slider-left-btn')
+let rightBtnSlider = document.getElementById('tasty-pancakes-content-slider-right-btn')
+let sliderImg = document.getElementById('start-img-tasty-pancakes-slider')
+
+let imgsSlider = 0
+
+rightBtnSlider.addEventListener('click', () => {
+    if(imgsSlider != 240) {
+        imgsSlider = imgsSlider + 120
+        sliderImg.style.paddingLeft = imgsSlider + 'px'
+        return
+    } 
+    console.log(imgsSlider)
+})
+
+leftBtnSlider.addEventListener('click', () => {
+    if(imgsSlider != 0) {
+        imgsSlider = imgsSlider - 120
+        sliderImg.style.paddingLeft = imgsSlider + 'px'
+        return
+    } 
+    console.log(imgsSlider)
+})
